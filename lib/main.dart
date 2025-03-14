f4fa730d759e567e9b2f88e073d04d0d762fb68f
Pero Osama <peroosama03@gmail.com>
2025-03-14 03:13:11 +0200

    pero

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:device_preview/device_preview.dart';
 import 'package:petsica/core/constants.dart';
 import 'package:petsica/core/utils/app_router.dart';
 void main() {
-  // initial 
+  // perooooooo
   runApp(
     DevicePreview(
       enabled: false, // تفعيل معاينة الجهاز
